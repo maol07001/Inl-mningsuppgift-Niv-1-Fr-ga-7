ecd6a6e0fe86e32e485b64a2ab9fe4c20811dd44
mariu <mariu@DESKTOP-CT70FK7>
2024-12-16 16:42:29 +0100

    Add project files.

---
+++ Inlämningsuppgift-Nivå 1 Fråga 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nummer;
+
+            Console.Write("Skriv in ett positivt tal: ");
+            nummer = int.Parse(Console.ReadLine());
+
+            while (nummer >= 0)
+            {
+                Console.Write("Skriv in ett positivt tal: ");
+                nummer = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Programmet avslutas.");
+        }
+    }
+}
